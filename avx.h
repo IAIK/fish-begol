d45e98a36e851ac0b262d17fb59200f029f6ece6
Sebastian Ramacher <sebastian.ramacher@iaik.tugraz.at>
2016-09-01 14:21:15 +0200

    Check for non-zero count

--- avx.h
+++ avx.h
@@ -10,6 +10,10 @@
  * \brief Perform a left shift on a 256 bit value.
  */
 static inline __m256i FN_ATTRIBUTES_AVX2 mm256_shift_left(__m256i data, unsigned int count) {
+  if (!count) {
+    return data;
+  }
+
   __m256i carry  = _mm256_srli_epi64(data, 64 - count);
   __m256i rotate = _mm256_permute4x64_epi64(carry, _MM_SHUFFLE(2, 1, 0, 3));
   carry          = _mm256_blend_epi32(_mm256_setzero_si256(), rotate, _MM_SHUFFLE(3, 3, 3, 0));
@@ -21,6 +25,10 @@ static inline __m256i FN_ATTRIBUTES_AVX2 mm256_shift_left(__m256i data, unsigned
  * \brief Perform a right shift on a 256 bit value.
  */
 static inline __m256i FN_ATTRIBUTES_AVX2 mm256_shift_right(__m256i data, unsigned int count) {
+  if (!count) {
+    return data;
+  }
+
   __m256i carry  = _mm256_slli_epi64(data, 64 - count);
   __m256i rotate = _mm256_permute4x64_epi64(carry, _MM_SHUFFLE(0, 3, 2, 1));
   carry          = _mm256_blend_epi32(_mm256_setzero_si256(), rotate, _MM_SHUFFLE(0, 3, 3, 3));
@@ -32,6 +40,10 @@ static inline __m256i FN_ATTRIBUTES_AVX2 mm256_shift_right(__m256i data, unsigne
  * \brief Perform a left shift on a 128 bit value.
  */
 static inline __m128i FN_ATTRIBUTES_SSE2 mm128_shift_left(__m128i data, unsigned int count) {
+  if (!count) {
+    return data;
+  }
+
   __m128i carry  = _mm_srli_epi64(data, 64 - count);
   __m128i upper  = _mm_slli_si128(carry, 8);
   __m128i lower  = _mm_srli_si128(carry, 8);
@@ -44,6 +56,10 @@ static inline __m128i FN_ATTRIBUTES_SSE2 mm128_shift_left(__m128i data, unsigned
  * \brief Perform a right shift on a 128 bit value.
  */
 static inline __m128i FN_ATTRIBUTES_SSE2 mm128_shift_right(__m128i data, unsigned int count) {
+  if (!count) {
+    return data;
+  }
+
   __m128i carry  = _mm_slli_epi64(data, 64 - count);
   __m128i upper  = _mm_slli_si128(carry, 8);
   __m128i lower  = _mm_srli_si128(carry, 8);
