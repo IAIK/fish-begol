e2887e715d778edb67d851d0e440ee7046e7d936
David Derler <david.derler@iaik.tugraz.at>
2016-09-01 14:14:40 +0200

    fix

--- timing/timing.m
+++ timing/timing.m
@@ -1,26 +1,35 @@
 #! /bin/octave -qf
 
 function create_plot(prefix, args, data)
-  fig = figure;
-  plot(1:rows(data), data);
+  fig = figure('papersize', [1000, 1000]);
+  [ax, h1, h2] = plotyy(1:rows(data), data(:, 1:3), 1:rows(data), data(:,4));
+  set(h1(1), 'marker', '.','markersize', 8, 'color', 'blue', 'linewidth', 2);
+  set(h1(2), 'marker', 'diamond','markersize', 2, 'color', 'red', 'linewidth', 2);
+  set(h1(3), 'marker', 'square','markersize', 2, 'color', 'magenta', 'linewidth', 2);
+  set(h2, 'marker', '>','markersize', 2, 'color', 'green', 'linewidth', 2);
+  set(ax(1), 'ycolor', 'black');
+  set(ax(2), 'ycolor', 'black');
   grid on;
   title(strcat("LowMC Timing n=", args{1}, " k=", args{2}, "\n\n", "Parameters [m]-[r]"));
   xlabel(" ");
-  set(gca, 'xtick', 1:rows(data));
-  set(gca, 'xticklabel', "");  
+  set(ax, 'xtick', 1:rows(data));
+  set(ax, 'xticklabel', "");  
+
   xlim([1, rows(data)])
   for i = 1:rows(data)
     text(i, 0, args{i + 2}, "rotation", 60, "verticalalignment", "top", "horizontalalignment", "right");
   endfor
-  ylabel("Time [ms]");
-  legend("Instance Gen", "Sign", "Verify");
-  print(fig, strcat(prefix, "-", args{1}, "-", args{2}, ".png"), "-dpng");
+  set(get(ax(1),'Ylabel'),'String','Time [ms]');
+  set(get(ax(2),'Ylabel'),'String','Signature Size [kB]');
+  legend(ax(1), "Instance Gen", "Sign", "Verify");
+  set (gca, "position", [0.1 0.1 0.8 0.8]) 
+  print(fig, strcat(prefix, "-", args{1}, "-", args{2}, ".jpg"), "-djpg", "-loose");
 endfunction
 
 arg_list = argv();
 
-fiatshamir = zeros(nargin - 2, 13);
-bellargw   = zeros(nargin - 2, 13);
+fiatshamir = zeros(nargin - 2, 14);
+bellargw   = zeros(nargin - 2, 14);
 
 for i = 3:nargin
   data = csvread(arg_list{i}) / 1000;
@@ -29,8 +38,8 @@ for i = 3:nargin
   bellargw(i - 2, :) = sum(data(numr+1:rows(data) ,:),1)/numr;
 endfor
 
-fs_sum = zeros(nargin - 2, 3);
-bg_sum = zeros(nargin - 2, 3);
+fs_sum = zeros(nargin - 2, 4);
+bg_sum = zeros(nargin - 2, 4);
 
 fs_sum(:,1) = sum(fiatshamir(:,1:3), 2);
 bg_sum(:,1) = sum(bellargw(:,1:3), 2);
@@ -38,6 +47,8 @@ fs_sum(:,2) = sum(fiatshamir(:,4:8), 2);
 bg_sum(:,2) = sum(bellargw(:,4:8), 2);
 fs_sum(:,3) = sum(fiatshamir(:,9:13), 2);
 bg_sum(:,3) = sum(bellargw(:,9:13), 2);
+fs_sum(:,4) = sum(fiatshamir(:,14), 2);
+bg_sum(:,4) = sum(bellargw(:,14), 2);
 
 
 create_plot("fis", arg_list, fs_sum);
