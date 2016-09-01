a8ce21462723aabb108eae23d6e654d1cf05a0bb
Sebastian Ramacher <sebastian.ramacher@iaik.tugraz.at>
2016-09-01 16:56:25 +0200

    Fix plot

--- timing/timing.m
+++ timing/timing.m
@@ -1,5 +1,37 @@
 #! /bin/octave -qf
 
+% from http://stackoverflow.com/questions/2219208/aquaterm-titles-and-axis-labels-getting-cut-off
+function fixAxes
+%---------------------------------------
+%// Kludge to fix scaling of all figures
+%// until GNU or I can find real fix.
+%// Octave3.2.3 computes the scaling wrong
+%// for this mac, such that the title 
+%// and xlabel are not displayed.
+%---------------------------------------
+s = get(0,'showhiddenhandles');
+set(0,'showhiddenhandles','on');
+newpos = [0.13 0.135 0.775 0.75];        %// default is [0.13 0.11 0.775 0.815]
+figs = get(0,'children');
+if (~isempty(figs))
+    for k=1:length(figs)
+        cax = get(figs(k),'currentaxes');
+        pos = get(cax,'position');       
+        if ~(pos(1) == newpos(1) && ... 
+             pos(2) == newpos(2) && ...
+             pos(3) == newpos(3) && ...
+             pos(4) == newpos(4))
+            set(cax,'position',newpos);    
+            set(0,'currentfigure',figs(k));
+            drawnow();
+        endif
+    endfor
+endif
+set(0,'showhiddenhandles',s);
+%---------------------------------------
+endfunction
+%---------------------------------------
+
 function create_plot(prefix, args, data)
   fig = figure;
   [ax, h1, h2] = plotyy(1:rows(data), data(:, 1:3), 1:rows(data), data(:,4));
@@ -22,7 +54,8 @@ function create_plot(prefix, args, data)
   set(get(ax(1),'Ylabel'),'String','Time [ms]');
   set(get(ax(2),'Ylabel'),'String','Signature Size [kB]');
   legend(ax(1), "Instance Gen", "Sign", "Verify");
-  set (gca, "position", [0.1 0.1 0.8 0.8]) 
+  # set (gca, "position", [0.1 0.1 0.8 0.8]) 
+  fixAxes
   print(fig, strcat(prefix, "-", args{1}, "-", args{2}, ".png"), "-dpng", "-loose");
 endfunction
 
