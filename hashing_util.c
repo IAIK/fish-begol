2b0d1ed1595a6c5e6b084d812dcaba9201c4fab4
Sebastian Ramacher <sebastian.ramacher@iaik.tugraz.at>
2016-09-12 15:10:25 +0200

    Improve hashing

--- hashing_util.c
+++ hashing_util.c
@@ -2,7 +2,7 @@
 #include "mpc_lowmc.h"
 #include <m4ri/m4ri.h>
 
-static void hash_mzd(SHA256_CTX* ctx, mzd_t* v) {
+static void hash_mzd(SHA256_CTX* ctx, mzd_t const* v) {
   const rci_t nrows = v->nrows;
   for (rci_t m = 0; m < nrows; ++m) {
     SHA256_Update(ctx, v->rows[m], sizeof(word) * v->width);
@@ -13,8 +13,8 @@ static void hash_mzd(SHA256_CTX* ctx, mzd_t* v) {
  * Computes the SHA256 hash of a view using openssl (similar as in
  * https://github.com/Sobuno/ZKBoo/blob/master/MPC_SHA256/shared.h)
  */
-void H(unsigned char k[16], mzd_t* y[3], view_t* v, unsigned vidx, unsigned vcnt,
-       unsigned char r[4], unsigned char hash[SHA256_DIGEST_LENGTH]) {
+void H(const unsigned char k[16], mzd_t* y[3], const view_t* v, unsigned vidx, unsigned vcnt,
+       const unsigned char r[4], unsigned char hash[SHA256_DIGEST_LENGTH]) {
   SHA256_CTX ctx;
   SHA256_Init(&ctx);
   SHA256_Update(&ctx, k, 16);
@@ -115,8 +115,8 @@ void bg_H3_verify(unsigned char h1[NUM_ROUNDS][2][SHA256_DIGEST_LENGTH],
   bg_H3(tmp1, tmp2, ch);
 }
 
-void bg_H3(unsigned char h1[NUM_ROUNDS][3][SHA256_DIGEST_LENGTH],
-        unsigned char h2[NUM_ROUNDS][3][SHA256_DIGEST_LENGTH], int* ch) {
+void bg_H3(const unsigned char h1[NUM_ROUNDS][3][SHA256_DIGEST_LENGTH],
+        const unsigned char h2[NUM_ROUNDS][3][SHA256_DIGEST_LENGTH], int* ch) {
   unsigned char hash[SHA256_DIGEST_LENGTH];
   SHA256_CTX ctx;
   SHA256_Init(&ctx);
@@ -125,9 +125,9 @@ void bg_H3(unsigned char h1[NUM_ROUNDS][3][SHA256_DIGEST_LENGTH],
   SHA256_Final(hash, &ctx);
 
   // Pick bits from hash
-  unsigned int i          = 0;
+  int* eof = ch + NUM_ROUNDS;
   unsigned int bitTracker = 0;
-  while (i < NUM_ROUNDS) {
+  while (ch < eof) {
     if (bitTracker >= SHA256_DIGEST_LENGTH * 8) { // Generate new hash
       SHA256_Init(&ctx);
       SHA256_Update(&ctx, hash, sizeof(hash));
@@ -138,7 +138,7 @@ void bg_H3(unsigned char h1[NUM_ROUNDS][3][SHA256_DIGEST_LENGTH],
 
     unsigned char twobits = (hash[bitTracker / 8] >> (bitTracker % 8)) & 0x3;
     if (twobits != 0x3) {
-      ch[i++] = twobits;
+      *ch++ = twobits;
     }
     bitTracker += 2;
   }
--- hashing_util.h
+++ hashing_util.h
@@ -6,8 +6,8 @@
 
 #define GETBIT(x, i) (((x) >> (i)) & 0x01)
 
-void H(unsigned char k[16], mzd_t *y[3], view_t* v, unsigned vidx, 
-    unsigned vcnt, unsigned char r[4], 
+void H(const unsigned char k[16], mzd_t *y[3], view_t const* v, unsigned vidx, 
+    unsigned vcnt, const unsigned char r[4],
     unsigned char hash[SHA256_DIGEST_LENGTH]);
 
 void fis_H3(unsigned char h[NUM_ROUNDS][3][SHA256_DIGEST_LENGTH], char *m, 
@@ -18,8 +18,8 @@ void fis_H3_verify(unsigned char h[NUM_ROUNDS][2][SHA256_DIGEST_LENGTH],
                 unsigned char ch_in[(NUM_ROUNDS + 3) / 4], char *m, 
                 unsigned m_len, int* ch);
 
-void bg_H3(unsigned char c1[NUM_ROUNDS][3][SHA256_DIGEST_LENGTH], 
-        unsigned char c2[NUM_ROUNDS][3][SHA256_DIGEST_LENGTH], int* ch);
+void bg_H3(const unsigned char c1[NUM_ROUNDS][3][SHA256_DIGEST_LENGTH], 
+        const unsigned char c2[NUM_ROUNDS][3][SHA256_DIGEST_LENGTH], int* ch);
 
 void bg_H3_verify(unsigned char h1[NUM_ROUNDS][2][SHA256_DIGEST_LENGTH], 
                unsigned char hp1[NUM_ROUNDS][SHA256_DIGEST_LENGTH], 
