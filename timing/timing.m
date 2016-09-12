8cbfa0648eaaec259d75419edf2bcfb0e3f89482
Sebastian Ramacher <sebastian.ramacher@iaik.tugraz.at>
2016-09-12 16:41:53 +0200

    Let timing.py generate mat files which are processed by octave script

--- timing/timing.m
+++ timing/timing.m
@@ -16,12 +16,12 @@ figs = get(0,'children');
 if (~isempty(figs))
     for k=1:length(figs)
         cax = get(figs(k),'currentaxes');
-        pos = get(cax,'position');       
-        if ~(pos(1) == newpos(1) && ... 
+        pos = get(cax,'position');
+        if ~(pos(1) == newpos(1) && ...
              pos(2) == newpos(2) && ...
              pos(3) == newpos(3) && ...
              pos(4) == newpos(4))
-            set(cax,'position',newpos);    
+            set(cax,'position',newpos);
             set(0,'currentfigure',figs(k));
             drawnow();
         endif
@@ -32,7 +32,7 @@ set(0,'showhiddenhandles',s);
 endfunction
 %---------------------------------------
 
-function create_plot(prefix, args, data)
+function create_plot(prefix, n, k, labels, data)
   fig = figure;
   [ax, h1, h2] = plotyy(1:rows(data), data(:, 1:3), 1:rows(data), data(:,4));
   set(h1(1), 'marker', '.','markersize', 8, 'color', 'blue', 'linewidth', 2);
@@ -42,36 +42,34 @@ function create_plot(prefix, args, data)
   set(ax(1), 'ycolor', 'black');
   set(ax(2), 'ycolor', 'black');
   grid on;
-  title(strcat("LowMC Timing n=", args{1}, " k=", args{2}, "\n\n", "Parameters [m]-[r]"));
+  title(strcat("LowMC Timing n=", n, " k=", k, "\n\n", "Parameters [m]-[r]"));
   xlabel(" ");
   set(ax, 'xtick', 1:rows(data));
   set(ax, 'xticklabel', "");  
 
   xlim([1, rows(data)])
   for i = 1:rows(data)
-    text(i, 0, args{i + 2}, "rotation", 60, "verticalalignment", "top", "horizontalalignment", "right");
+    text(i, 0, labels(i,:), "rotation", 60, "verticalalignment", "top", "horizontalalignment", "right");
   endfor
   set(get(ax(1),'Ylabel'),'String','Time [ms]');
   set(get(ax(2),'Ylabel'),'String','Signature Size [kB]');
   legend(ax(1), "Instance Gen", "Sign", "Verify");
   fixAxes
-  print(fig, strcat(prefix, "-", args{1}, "-", args{2}, ".png"), "-dpng", "-loose");
+  print(fig, strcat(prefix, "-", n, "-", k, ".png"), "-dpng", "-loose");
 endfunction
 
 arg_list = argv();
 
-fiatshamir = zeros(nargin - 2, 13);
-bellargw   = zeros(nargin - 2, 13);
+n = arg_list{1};
+k = arg_list{2};
 
-for i = 3:nargin
-  data = csvread(arg_list{i}) / 1000;
-  numr = rows(data)/2;
-  fiatshamir(i - 2, :) = sum(data(1:numr,:),1)/numr;
-  bellargw(i - 2, :) = sum(data(numr+1:rows(data) ,:),1)/numr;
-endfor
+data = load(strcat("timings-", n, "-", k, ".mat"));
 
-fs_sum = zeros(nargin - 2, 4);
-bg_sum = zeros(nargin - 2, 4);
+fiatshamir = transpose(data.fis_sum) / 1000;
+bellargw = transpose(data.bg_sum) / 1000;
+
+fs_sum = zeros(rows(data.labels), 4);
+bg_sum = zeros(rows(data.labels), 4);
 
 fs_sum(:,1) = sum(fiatshamir(:,1:3), 2);
 bg_sum(:,1) = sum(bellargw(:,1:3), 2);
@@ -82,7 +80,6 @@ bg_sum(:,3) = sum(bellargw(:,9:12), 2);
 fs_sum(:,4) = sum(fiatshamir(:,13), 2);
 bg_sum(:,4) = sum(bellargw(:,13), 2);
 
-
-create_plot("fis", arg_list, fs_sum);
-create_plot("bg", arg_list, bg_sum);
+create_plot("fis", n, k, data.labels, fs_sum);
+create_plot("bg", n, k, data.labels, bg_sum);
 
--- timing/timing.py
+++ timing/timing.py
@@ -1,24 +1,49 @@
 import argparse
 import subprocess
 import os
+import numpy as np
+import h5py
 
 def main():
   args = parse_args()
   k = args.keysize
   with open(args.filename) as f:
     octarg = []
+    all_timings_fs = []
+    all_timings_bg = []
     for line in f.readlines():
       if line.rstrip():
         m, n, r = get_params(line)
         fname = "{0}-{1}".format(m, r)
         octarg.append(fname)
+
         with open(fname, "w") as timings:
-          subprocess.Popen("./{0} {1} {2} {3} {4} {5}".format(args.executable, 
-                           m, n, r, k, args.iterations), shell=True, stdout=timings).wait()
-    subprocess.Popen("octave {0} {1} {2} {3}".format(args.octavescript, n, k,
-                                                     " ".join(octarg)), shell=True).wait()
-    for fname in octarg:
+          subprocess.Popen([args.executable, str(m), str(n), str(r), str(k),
+                            str(args.iterations)], stdout=timings).wait()
+
+        with open(fname, "r") as timings:
+          data = timings.read()
         os.unlink(fname)
+
+        mat = []
+        for line in data.split("\n"):
+          line = line.replace('{', '')
+          line = line.replace('}', '')
+          if not len(line):
+            continue
+
+          mat.append(map(int, line.split(',')))
+
+        fs = np.array(mat[:len(mat) / 2])
+        bg = np.array(mat[len(mat) / 2:])
+
+        all_timings_fs.append(np.sum(fs, axis=0) / float(args.iterations))
+        all_timings_bg.append(np.sum(bg, axis=0) / float(args.iterations))
+
+    with h5py.File('timings-{0}-{1}.mat'.format(n, k), 'w') as timings:
+        timings.create_dataset("fis_sum", data=np.array(all_timings_fs))
+        timings.create_dataset("bg_sum", data=np.array(all_timings_bg))
+        timings.create_dataset('labels', data=octarg)
 
 def get_params(line):
   l = line.split()
@@ -27,11 +52,11 @@ def get_params(line):
   n = d['blocksize:']
   r = d['ANDdepth:']
   return m, n, r
-  
+
 def parse_args():
   parser = argparse.ArgumentParser(description='Process LowMC Timing Args.')
-  parser.add_argument("filename", help="the filename of the lowmc instances")  
-  parser.add_argument("-k", "--keysize", help="the LowMC keysize", type=int, 
+  parser.add_argument("filename", help="the filename of the lowmc instances")
+  parser.add_argument("-k", "--keysize", help="the LowMC keysize", type=int,
                       choices=[128, 192, 256, 384, 448, 512], default=128)
   parser.add_argument("-x", "--executable", help="the LowMC MPC executable",
                       default="../mpc_lowmc")
