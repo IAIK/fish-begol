ff5d69b64c63ff2e76f01b7dc0485e98d0cfeda7
Sebastian Ramacher <sebastian.ramacher@iaik.tugraz.at>
2016-09-18 15:02:00 +0200

    Avoid looping mallocs

--- signature_common.c
+++ signature_common.c
@@ -18,16 +18,21 @@ void destroy_instance(public_parameters_t* pp) {
 void init_view(mpc_lowmc_t const* mpc_lowmc, view_t* views[NUM_ROUNDS]) {
   const unsigned int view_count = 2 + mpc_lowmc->r;
 
-  for (unsigned int i = 0; i < NUM_ROUNDS; i++) {
-    views[i] = calloc(view_count, sizeof(view_t*));
+  unsigned char* buffer = calloc(view_count * NUM_ROUNDS, sizeof(view_t*) + 3 * sizeof(mzd_t*));
 
-    views[i][0].s = calloc(3, sizeof(mzd_t*));
+  for (unsigned int i = 0; i < NUM_ROUNDS; i++) {
+    views[i] = (view_t*) buffer;
+    buffer += view_count * sizeof(view_t*);
+
+    views[i][0].s = (mzd_t**) buffer;
+    buffer += 3 * sizeof(mzd_t*);
     for (unsigned m = 0; m < 3; m++) {
       views[i][0].s[m] = mzd_local_init(1, mpc_lowmc->k);
     }
 
     for (unsigned n = 1; n < view_count; n++) {
-      views[i][n].s = (mzd_t**)malloc(3 * sizeof(mzd_t*));
+      views[i][n].s = (mzd_t**) buffer;
+      buffer += 3 * sizeof(mzd_t*);
       for (unsigned m = 0; m < 3; m++) {
         views[i][n].s[m] = mzd_local_init(1, mpc_lowmc->n);
       }
@@ -36,12 +41,5 @@ void init_view(mpc_lowmc_t const* mpc_lowmc, view_t* views[NUM_ROUNDS]) {
 }
 
 void free_view(mpc_lowmc_t const* mpc_lowmc, view_t* views[NUM_ROUNDS]) {
-  const unsigned int view_count = 2 + mpc_lowmc->r;
-
-  for (unsigned int j = 0; j < NUM_ROUNDS; ++j) {
-    for (unsigned i = 0; i < view_count; ++i) {
-      free(views[j][i].s);
-    }
-    free(views[j]);
-  }
+  free(views[0]);
 }
