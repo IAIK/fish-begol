d6782ea12d4edb20d0c5e2256e4a86dc5c3cce76
Sebastian Ramacher <sebastian.ramacher@iaik.tugraz.at>
2016-09-22 17:12:55 +0200

    Update

--- mzd_additional.h
+++ mzd_additional.h
@@ -4,8 +4,6 @@
 #include "randomness.h"
 #include <m4ri/m4ri.h>
 
-// #ifdef WITH_OPENMP
-
 /**
  * Modified mzd_init calling malloc less often. Do not pass mzd_t instances
  * initialized with this function to mzd_free.
@@ -27,11 +25,6 @@ void mzd_local_free_multiple(mzd_t** vs);
  * Improved mzd_copy for specific memory layouts.
  */
 mzd_t* mzd_local_copy(mzd_t* dst, mzd_t const* src);
-// #else
-// #define mzd_local_init mzd_init
-// #define mzd_local_free mzd_free
-// #define mzd_local_copy mzd_copy
-// #endif
 
 /**
  * Initializes a random vector
@@ -81,11 +74,17 @@ typedef struct {
   mzd_t* shared[3];
 } mzd_shared_t;
 
-#define MZD_SHARED_EMPTY {0, { NULL }}
+#define MZD_SHARED_EMPTY                                                                           \
+  {                                                                                                \
+    0, {                                                                                           \
+      NULL                                                                                         \
+    }                                                                                              \
+  }
 
 void mzd_shared_init(mzd_shared_t* shared_value, mzd_t const* value);
 void mzd_shared_copy(mzd_shared_t* dst, mzd_shared_t const* src);
-void mzd_shared_from_shares(mzd_shared_t* shared_value, mzd_t* const* shares, unsigned int share_count);
+void mzd_shared_from_shares(mzd_shared_t* shared_value, mzd_t* const* shares,
+                            unsigned int share_count);
 void mzd_shared_share(mzd_shared_t* shared_value);
 void mzd_shared_share_prng(mzd_shared_t* shared_value, aes_prng_t* aes_prng);
 void mzd_shared_clear(mzd_shared_t* shared_value);
