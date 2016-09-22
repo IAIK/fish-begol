8add54005e9289200b949b7007c0b4ba5d1b8b3e
Sebastian Ramacher <sebastian.ramacher@iaik.tugraz.at>
2016-09-22 11:07:28 +0200

    Parameter desc

--- parameters.h
+++ parameters.h
@@ -3,8 +3,11 @@
 
 #include <openssl/sha.h>
 
+// Output of size of the random oracle (\gamma')
 #define COMMITMENT_LENGTH SHA256_DIGEST_LENGTH
 #define COMMITMENT_RAND_LENGTH 4
+
+// Repetition count (\gamma)
 #define NUM_ROUNDS 219
 #define FIS_NUM_ROUNDS NUM_ROUNDS
 #define BG_NUM_ROUNDS NUM_ROUNDS
