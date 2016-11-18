6eb907546bd0181007c28dd3e0faaafdc859bb9d
Sebastian Ramacher <sebastian.ramacher@iaik.tugraz.at>
2016-11-18 13:44:50 +0100

    Improved OpenSSL compatibility

--- multithreading.c
+++ multithreading.c
@@ -1,13 +1,14 @@
 #include "multithreading.h"
 
+#include <openssl/crypto.h>
+
 /**
  * OpenMP integration of OpenSSL is based on
  * https://github.com/Sobuno/ZKBoo/blob/master/MPC_SHA256/shared.h.
  */
 
-#ifdef WITH_OPENMP
+#if defined(WITH_OPENMP) && OPENSSL_VERSION_NUMBER < 0x10100000
 #include <omp.h>
-#include <openssl/crypto.h>
 
 static omp_lock_t* locks = NULL;
 
@@ -21,9 +22,11 @@ static void openmp_locking_callback(int mode, int type, const char* file, int li
   }
 }
 
+#if OPENSSL_VERSION_NUMBER < 0x10000000
 static unsigned long openmp_thread_id(void) {
   return (unsigned long)omp_get_thread_num();
 }
+#endif
 
 void openmp_thread_setup(void) {
   locks = OPENSSL_malloc(CRYPTO_num_locks() * sizeof(omp_lock_t));
@@ -31,7 +34,9 @@ void openmp_thread_setup(void) {
     omp_init_lock(&locks[i]);
   }
 
+#if OPENSSL_VERSION_NUMBER < 0x10000000
   CRYPTO_set_id_callback(openmp_thread_id);
+#endif
   CRYPTO_set_locking_callback(openmp_locking_callback);
 }
 
