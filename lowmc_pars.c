da58f8ce1e24b629bb2533727bf8d71333a9a459
Sebastian Ramacher <sebastian.ramacher@iaik.tugraz.at>
2017-06-30 13:27:06 +0200

    Write only essential data

--- lowmc_pars.c
+++ lowmc_pars.c
@@ -188,13 +188,6 @@ void writeMZD_TStructToFile(mzd_t* matrix, FILE* file) {
 
   fwrite(&(matrix->nrows), sizeof(rci_t), 1, file);
   fwrite(&(matrix->ncols), sizeof(rci_t), 1, file);
-  fwrite(&(matrix->width), sizeof(wi_t), 1, file);
-  fwrite(&(matrix->rowstride), sizeof(wi_t), 1, file);
-  fwrite(&(matrix->offset_vector), sizeof(wi_t), 1, file);
-  fwrite(&(matrix->row_offset), sizeof(wi_t), 1, file);
-  fwrite(&(matrix->flags), sizeof(uint8_t), 1, file);
-  fwrite(&(matrix->blockrows_log), sizeof(uint8_t), 1, file);
-  fwrite(&(matrix->high_bitmask), sizeof(word), 1, file);
 
   for (int i = 0; i < matrix->nrows; i++) {
     fwrite((matrix->rows[i]), matrix->rowstride * sizeof(word), 1, file);
@@ -206,56 +199,10 @@ mzd_t* readMZD_TStructFromFile(FILE* file) {
   int ret       = 0;
   int nrows     = 0;
   int ncols     = 0;
-  int width     = 0;
-  int rowstride = 0;
   ret += fread(&(nrows), sizeof(rci_t), 1, file);
   ret += fread(&(ncols), sizeof(rci_t), 1, file);
-  ret += fread(&(width), sizeof(wi_t), 1, file);
-  ret += fread(&(rowstride), sizeof(wi_t), 1, file);
 
-  const rci_t width_cal = (ncols + m4ri_radix - 1) / m4ri_radix;
-  rci_t rowstride_cal   = 0;
-
-  // COPIED FROM MZD_ADDITIONAL.C
-  if ((size_t)width_cal >= (256 / (8 * sizeof(word)))) {
-    rowstride_cal = ((width_cal * sizeof(word) + 31) & ~31) / sizeof(word);
-  } else {
-    rowstride_cal = ((width_cal * sizeof(word) + 15) & ~15) / sizeof(word);
-  }
-
-  if (width != width_cal || rowstride != rowstride_cal) {
-    printf("Error when reading file!\n");
-  }
-
-  const size_t buffer_size = nrows * rowstride * sizeof(word);
-  const size_t rows_size   = nrows * sizeof(word*);
-
-  static const size_t mzd_t_size = (sizeof(mzd_t) + 0x3f) & ~0x3f;
-  unsigned char* buffer = aligned_alloc(32, (mzd_t_size + buffer_size + rows_size + 31) & ~31);
-
-  mzd_t* A = (mzd_t*)buffer;
-  buffer += mzd_t_size;
-
-  memset(buffer, 0, buffer_size);
-
-  A->nrows     = nrows;
-  A->ncols     = ncols;
-  A->width     = width;
-  A->rowstride = rowstride;
-
-  ret += fread(&(A->offset_vector), sizeof(wi_t), 1, file);
-  ret += fread(&(A->row_offset), sizeof(wi_t), 1, file);
-  ret += fread(&(A->flags), sizeof(uint8_t), 1, file);
-  ret += fread(&(A->blockrows_log), sizeof(uint8_t), 1, file);
-  ret += fread(&(A->high_bitmask), sizeof(word), 1, file);
-
-  A->blocks = NULL;
-
-  A->rows = (word**)(buffer + buffer_size);
-  for (rci_t i = 0; i < nrows; ++i, buffer += rowstride * sizeof(word)) {
-    A->rows[i] = (word*)(buffer);
-  }
-
+  mzd_t* A = mzd_local_init_ex(nrows, ncols, false);
   for (int i = 0; i < A->nrows; i++) {
     ret += fread((A->rows[i]), A->rowstride * sizeof(word), 1, file);
   }
